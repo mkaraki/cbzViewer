58201518a2e8507320b9d8cbffde8de403c7c682
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-08-10 01:16:24 +0900

    Add legal view in server

--- server.go
+++ server.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 var conf *config
+
+func legalHandler(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("templates/legal.html")
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, err = io.Copy(w, f)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println(err)
+		return
+	}
+}
 
 func main() {
 	var err error
@@ -18,6 +37,8 @@ func main() {
 	http.HandleFunc("/list", listHandler)
 	http.HandleFunc("/read", readHandler)
 	http.HandleFunc("/img", imgHandler)
+
+	http.HandleFunc("/legal", legalHandler)
 
 	fs := http.FileServer(http.Dir("templates/assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
