0327e30c41c85b30087fe5a3ba5e663be20876e5
mkaraki <git@su.mkarakiapps.com>
2026-03-29 00:05:56 +0900

    Fix cypress test

--- frontend/cypress/e2e/list.cy.ts
+++ frontend/cypress/e2e/list.cy.ts
@@ -29,20 +29,20 @@ describe('List view check', () => {
         cy.visit('/list?path=')
         cy.contains('tests').should('exist');
         cy.contains('tests').click();
-        cy.url().should('include', '/list?path=%2Ftests')
+        cy.url().should('include', '/list?path=tests%2F')
     })
 
     it('check tests dir exists with non standard link', () => {
         cy.visit('/list')
         cy.contains('tests').should('exist');
         cy.contains('tests').click();
-        cy.url().should('include', '/list?path=%2Ftests')
+        cy.url().should('include', '/list?path=tests%2F')
 
         cy.visit('/list?path=%2F')
         cy.contains('tests').should('exist');
         cy.contains('tests').click();
         // ToDo: Should normalize
-        cy.url().should('include', '/list?path=%2Ftests')
+        cy.url().should('include', '/list?path=tests%2F')
     })
 
     it('check parent dir button works', () => {
@@ -77,7 +77,7 @@ describe('List view check', () => {
         cy.visit('/list?path=tests')
         cy.contains('Testing Introduction 01.cbz').click();
 
-        cy.url().should('include', '/read?path=%2Ftests%2FTesting%20Introduction%2001.cbz');
+        cy.url().should('include', '/read?path=tests%2FTesting%20Introduction%2001.cbz');
     });
 });
 
